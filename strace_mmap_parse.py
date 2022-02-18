354edc7696fdb01bbb62a9394fbefa567acb99f2
thornbird <thornbird.blue@gmail.com>
2022-02-18 17:18:41 +0800

    ci base test ok code

--- strace_mmap_parse.py
+++ strace_mmap_parse.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 
 import sys,re
+import pandas as pd
 
-parse_file=''
+file_name=''
 
-pid=0
 threads=0
 
-strace_reg='strace: Process (\d+) attached with (\d+) threads'
-mmap_reg='mmap(\S+, (\d+),'
+strace_reg='strace: Process \d+ attached with (\d+) threads'
+
+pid_reg='\[pid\s+(\d+)\]'
+time_reg='\d{2}:\d{2}:\d{2}.\d{6}'
+mmap_reg='mmap\(\S+, (\d+),'
+
+mmap=dict()
 
 def mmap_parse(l):
-	print(l)
+	r=re.match(pid_reg,l)
+	if r!=None:
+		pid=r.group(1)
+
+		r=re.search(time_reg,l)
+		if r!=None:
+			time=r.group(0)
+
+		r=re.search(mmap_reg,l)
+		if r!=None:
+			mmap_len =r.group(1)
+			
+			if pid in mmap:
+				mmap[pid]+=int(mmap_len)
+			else:
+				mmap[pid]=int(mmap_len)
 
 def parse_file(fh):
 	while 1:
@@ -22,17 +42,24 @@ def parse_file(fh):
 		
 		mmap_parse(line)
 
-	close(fh)		
+	fh.close()		
 	print("Finish Parse File!!!")
 
-def get_file_handle(f_name):
-	with open(f_name,'r') as f:
-		line=fh.readline()
-		if not line:
-			print("!!!Error!!! File is NULL!")
-			sys.exit(0)
+	df=pd.DataFrame.from_dict(mmap.orient='index').T
+	print(df)
+	df.to_excel("parse.xlsx",index=False)
+	print(df.apply(lambda x:x.sum(),axis=1))
 
-		pid,threads=re.match(strace_reg,line)
+def get_file_handle(f_name):
+	f=open(f_name,'r')
+	line=fh.readline()
+	if not line:
+		print("!!!Error!!! File is NULL!")
+		sys.exit(0)
+
+	r=re.match(strace_reg,line)
+	if r!=None:
+		threads=r.group(1)
 		print("Threads: "+str(threads))
 		return f	
 
